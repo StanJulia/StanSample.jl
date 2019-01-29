f178b1687b71862204523da41781fbdab00bc60d
Tamas K. Papp <tkpapp@gmail.com>
2019-01-29 20:23:07 +0100

    Docstrings.

--- src/StanRun.jl
+++ src/StanRun.jl
@@ -1,3 +1,9 @@
+"""
+Helper infrastructure to compile and sample models using `cmdstan`.
+
+[`StanModel`](@ref) wraps a model definition (source code), while [`stan_sample`](@ref) can
+be used to sample from it.
+"""
 module StanRun
 
 export StanModel, stan_sample
@@ -28,6 +34,8 @@ string (not necessarily an extension, with the dot).
 
 When `verified_ext` is given, it the original extension is checked to be equivalent.
 Defaults to `".stan"`.
+
+Internal, not exported.
 """
 function replace_ext(path::AbstractString, new_ext, verified_ext = ".stan")
     basename, ext = splitext(path)
@@ -39,6 +47,8 @@ end
 $(SIGNATURES)
 
 Executable path corresponding to a source file, or a model.
+
+Internal, not exported.
 """
 function executable_path(source_path::AbstractString)
     replace_ext(source_path, Sys.iswindows() ? ".exe" : "")
@@ -46,10 +56,28 @@ end
 
 executable_path(model::StanModel) = executable_path(model.source_path)
 
+"""
+$(SIGNATURES)
+
+Define a model by its Stan source code location, which needs to end in `".stan"`.
+
+Its directory needs to be *writable*, as it will contain the compiled executable (generated
+on demand if it does not exist, or if the source code is more recent).
+
+`cmdstan_home` should specify the directory of the `cmdstan` installation. The default is
+obtained from the environment variable `$(CMDSTAN_HOME_VAR)`."
+"""
 function StanModel(source_path; cmdstan_home = get_cmdstan_home())
     StanModel(source_path, cmdstan_home)
 end
 
+"""
+$(SIGNATURES)
+
+Ensure that a compiled model executable exists, and return its path.
+
+Internal, not exported.
+"""
 function ensure_executable(model::StanModel)
     @unpack cmdstan_home = model
     exec_path = executable_path(model)
@@ -59,8 +87,25 @@ function ensure_executable(model::StanModel)
     exec_path
 end
 
+"""
+$(SIGNATURES)
+
+Default `output_base`, in the same directory as the model. Internal, not exported.
+"""
 default_output_base(model::StanModel) = replace_ext(model.source_path, "", ".stan")
 
+"""
+$(SIGNATURES)
+
+Sample `n_chains` from `model` using `data`. Return the full paths of the sample files.
+
+`output_base` is used to write the data file (using `StanDump.stan_dump`) and to determine
+the resulting names for the sampler output. It defaults to the source file name without the
+extension.
+
+When `rm_samples` (default: `true`), remove potential pre-existing sample files after
+compiling the model.
+"""
 function stan_sample(model::StanModel, data, n_chains;
                      output_base = default_output_base(model),
                      data_file = output_base * ".data.R",
@@ -80,6 +125,8 @@ $(SIGNATURES)
 
 Return filenames of CSV files (with MCMC samples, this is not checked) matching
 `output_base` from the model.
+
+Part of the API, but not exported.
 """
 function find_samples(output_base::AbstractString)
     dir, basename = splitdir(output_base)
