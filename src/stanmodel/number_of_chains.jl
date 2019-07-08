95edf0b5baf8c63a115c8c9f7ef85cb5983d7aa4
Rob J Goedman <goedman@mac.com>
2019-07-08 10:27:04 -0600

    Update number_of_chains.jl

--- src/stanmodel/number_of_chains.jl
+++ src/stanmodel/number_of_chains.jl
@@ -1,7 +1,7 @@
-function get_n_chains(model::CmdStanSampleModel)
+function get_n_chains(model::CmdStanModel)
   model.n_chains[1]
 end
 
-function set_n_chains(model::CmdStanSampleModel, n_chains)
+function set_n_chains(model::CmdStanModel, n_chains)
   model.n_chains[1] = n_chains
 end
