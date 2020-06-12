d93ba8e845abbe61ed3771274982d00c1f1642a1
Rob J Goedman <goedman@mac.com>
2020-06-12 15:37:16 -0600

    Rel 2.1.6

--- src/mcm.jl
+++ src/mcm.jl
@@ -5,10 +5,10 @@ import .MonteCarloMeasurements: Particles
 function convert_a3d(a3d_array, cnames, ::Val{:particles};
     start=1, kwargs...)
   
-  df = convert_a3d(a3d_array, cnames, Val(:dataframe))
+  df = convert_a3d(a3d_array, Symbol.(cnames), Val(:dataframe))
   d = Dict{Symbol, typeof(Particles(size(df, 1), Normal(0.0, 1.0)))}()
 
-  for var in names(df)
+  for var in Symbol.(names(df))
     mu = mean(df[:, var])
     sigma = std(df[:, var])
     d[var] = Particles(size(df, 1), Normal(mu, sigma))
@@ -22,7 +22,7 @@ function Particles(df::DataFrame)
 
   d = Dict{Symbol, typeof(Particles(size(df, 1), Normal(0.0, 1.0)))}()
 
-  for var in names(df)
+  for var in Symbol.(names(df))
     d[var] = Particles(df[:, var])
   end
 
