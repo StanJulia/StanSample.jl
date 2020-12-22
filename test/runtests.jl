37849d6e0d87a5ebe0b52a07e757edf91af84715
Rob J Goedman <goedman@mac.com>
2020-12-22 09:30:20 -0700

    Rel 2.2.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,87 +1,93 @@
 using StanSample, Test
 
-TestDir = @__DIR__
-tmpdir = mktempdir()
-println()
+if haskey(ENV, "JULIA_CMDSTAN_HOME")
 
-test_bernoulli = [
-  "test_bernoulli/test_bernoulli_array_01.jl",
-]
+  TestDir = @__DIR__
+  tmpdir = mktempdir()
+  println()
 
-@testset "Bernoulli_01 array tests" begin
-    include(joinpath(TestDir, "test_bernoulli/test_bernoulli_array_01.jl"))
-    if success(rc)
-      sdf = read_summary(sm)
+  test_bernoulli = [
+    "test_bernoulli/test_bernoulli_array_01.jl",
+  ]
+
+  @testset "Bernoulli_01 array tests" begin
+      include(joinpath(TestDir, "test_bernoulli/test_bernoulli_array_01.jl"))
+      if success(rc)
+        sdf = read_summary(sm)
+        @test sdf[sdf.parameters .== :theta, :mean][1] ≈ 0.33 rtol=0.05
+
+        (samples, parameters) = read_samples(sm; return_parameters=true)
+        @test size(samples) == (1000, 1, 6)
+        @test length(parameters) == 1
+
+        (samples, parameters) = read_samples(sm;
+          return_parameters=true, include_internals=true)
+        @test size(samples) == (1000, 8, 6)
+        @test length(parameters) == 8
+
+        samples = read_samples(sm; include_internals=true)
+        @test size(samples) == (1000, 8, 6)
+
+        samples = read_samples(sm)
+        @test size(samples) == (1000, 1, 6)
+      end
+
+      include(joinpath(TestDir, "test_bernoulli/test_bernoulli_array_02.jl"))
+      if success(rc)
+        sdf = read_summary(sm)
+        @test sdf[sdf.parameters .== :theta, :mean][1] ≈ 0.33 rtol=0.05
+
+        (samples, parameters) = read_samples(sm; return_parameters=true)
+        @test size(samples) == (500, 1, 4)
+        @test length(parameters) == 1
+
+        samples = read_samples(sm; include_internals=true)
+        @test size(samples) == (500, 8, 4)
+      end
+  end
+
+  basic_run_tests = [
+    "test_basic_runs/test_bernoulli_dict.jl",
+    "test_basic_runs/test_bernoulli_array_dict_1.jl",
+    "test_basic_runs/test_bernoulli_array_dict_2.jl",
+    "test_basic_runs/test_parse_interpolate.jl"
+  ]
+
+  @testset "Bernoulli basic run tests" begin
+    for test in basic_run_tests
+      println("\nTesting: $test.")
+      include(joinpath(TestDir, test))
       @test sdf[sdf.parameters .== :theta, :mean][1] ≈ 0.33 rtol=0.05
-
-      (samples, parameters) = read_samples(sm; return_parameters=true)
-      @test size(samples) == (1000, 1, 6)
-      @test length(parameters) == 1
-
-      (samples, parameters) = read_samples(sm;
-        return_parameters=true, include_internals=true)
-      @test size(samples) == (1000, 8, 6)
-      @test length(parameters) == 8
-
-      samples = read_samples(sm; include_internals=true)
-      @test size(samples) == (1000, 8, 6)
-
-      samples = read_samples(sm)
-      @test size(samples) == (1000, 1, 6)
     end
+    println()
+  end
 
-    include(joinpath(TestDir, "test_bernoulli/test_bernoulli_array_02.jl"))
-    if success(rc)
-      sdf = read_summary(sm)
-      @test sdf[sdf.parameters .== :theta, :mean][1] ≈ 0.33 rtol=0.05
+  sample_settings_tests = [
+    "test_sample_settings/test_bernoulli.jl"
+  ]
 
-      (samples, parameters) = read_samples(sm; return_parameters=true)
-      @test size(samples) == (500, 1, 4)
-      @test length(parameters) == 1
-
-      samples = read_samples(sm; include_internals=true)
-      @test size(samples) == (500, 8, 4)
+  @testset "Bernoulli Sample() settings tests" begin
+    for test in sample_settings_tests
+      println("\nTesting: $test.")
+      include(joinpath(TestDir, test))
+      @test stanmodel.method.adapt.delta ≈ 0.85 atol=0.01
     end
-end
-
-basic_run_tests = [
-  "test_basic_runs/test_bernoulli_dict.jl",
-  "test_basic_runs/test_bernoulli_array_dict_1.jl",
-  "test_basic_runs/test_bernoulli_array_dict_2.jl",
-  "test_basic_runs/test_parse_interpolate.jl"
-]
-
-@testset "Bernoulli basic run tests" begin
-  for test in basic_run_tests
-    println("\nTesting: $test.")
-    include(joinpath(TestDir, test))
-    @test sdf[sdf.parameters .== :theta, :mean][1] ≈ 0.33 rtol=0.05
+    println()
   end
-  println()
-end
 
-sample_settings_tests = [
-  "test_sample_settings/test_bernoulli.jl"
-]
+  generate_quantities_tests = [
+    "test_generate_quantities/test_generate_quantities.jl"
+  ]
 
-@testset "Bernoulli Sample() settings tests" begin
-  for test in sample_settings_tests
-    println("\nTesting: $test.")
-    include(joinpath(TestDir, test))
-    @test stanmodel.method.adapt.delta ≈ 0.85 atol=0.01
+  @testset "Generate_quantities tests" begin
+    for test in generate_quantities_tests
+      println("\nTesting: $test.")
+      include(joinpath(TestDir, test))
+    end
+    println()
   end
-  println()
-end
-
-generate_quantities_tests = [
-  "test_generate_quantities/test_generate_quantities.jl"
-]
-
-@testset "Generate_quantities tests" begin
-  for test in generate_quantities_tests
-    println("\nTesting: $test.")
-    include(joinpath(TestDir, test))
-  end
-  println()
+  
+else
+  println("\nJULIA_CMDSTAN_HOME not set. Skipping tests")
 end
 
