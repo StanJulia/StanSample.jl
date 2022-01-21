f88dcfb5c76caee5db3559699c92d6df8002b108
Rob J Goedman <goedman@icloud.com>
2022-01-21 08:27:41 -0700

    Rel 5.6.0 - Fixed stan_summary

--- src/StanSample.jl
+++ src/StanSample.jl
@@ -36,6 +36,7 @@ import StanBase: update_R_files
 import StanBase: data_file_path, init_file_path, sample_file_path
 import StanBase: generated_quantities_file_path, log_file_path
 import StanBase: diagnostic_file_path, setup_diagnostics
+import StanBase: stan_summary, read_summary
 
 function __init__()
   @require MonteCarloMeasurements="0987c9cc-fe09-11e8-30f0-b96dd679fdca" include("utils/particles.jl")
