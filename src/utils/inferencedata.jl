6fbdc582d0019aa4c5cf3e797b027b4871b99ba6
Rob J Goedman <goedman@icloud.com>
2022-11-16 16:06:14 -0500

    Rel 6.12.0 - Add commentary

--- src/utils/inferencedata.jl
+++ src/utils/inferencedata.jl
@@ -4,8 +4,10 @@ function inferencedata(m::SampleModel;
     posterior_predictive_symbol::Union{Nothing, Symbol} = :y_hat,
     kwargs...)
 
+    # Read in the draws as a NamedTuple with sample_stats included
     stan_nts = read_samples(m, :namedtuples; include_internals=true)
 
+    # Define the "proper" ArviZ names for the sample statistics group.
     sample_stats_key_map = (
         n_leapfrog__=:n_steps,
         treedepth__=:tree_depth,
@@ -16,34 +18,41 @@ function inferencedata(m::SampleModel;
         accept_stat__=:acceptance_rate,
     );
 
+    # If a log_likelihood_symbol is defined (!= nothing), remove it from the future posterior group
     if !isnothing(log_likelihood_symbol)
         sample_nts = NamedTuple{filter(∉([log_likelihood_symbol]), keys(stan_nts))}(stan_nts)
     end
-    
+
+    # If a posterior_predictive_symbol is defined (!= nothing), remove it from the future posterior group
     if !isnothing(posterior_predictive_symbol)
         sample_nts = NamedTuple{filter(∉([posterior_predictive_symbol]), keys(sample_nts))}(sample_nts)
     end
 
-    sample_nts = NamedTuple{filter(∉(keys(sample_stats_key_map)), keys(sample_nts))}(sample_nts)
+    # `sample_nts` now holds remaining parameters and the sample statistics
+    # Split in 2 separate NamedTuples: posterior_nts and sample_stats_nts
+    posterior_nts = NamedTuple{filter(∉(keys(sample_stats_key_map)), keys(sample_nts))}(sample_nts)
+    sample_stats_nts = NamedTuple{filter(∈(keys(sample_stats_key_map)), keys(sample_nts))}(sample_nts)
 
-    stan_stats_nts = NamedTuple{filter(∈(keys(sample_stats_key_map)), keys(stan_nts))}(stan_nts)
+    # Remap the names according to above sample_stats_key_map
+    sample_stats_nts_rekey = 
+        NamedTuple{map(Base.Fix1(getproperty, sample_stats_key_map), keys(sample_stats_nts))}(
+            values(sample_stats_nts))
 
-    stan_stats_nts_rekey = 
-        NamedTuple{map(Base.Fix1(getproperty, sample_stats_key_map), keys(stan_stats_nts))}(
-            values(stan_stats_nts))
+    # Create initial inferencedata object with 2 groups
+    idata = from_namedtuple(sample_nts; sample_stats=sample_stats_nts_rekey)
 
-    idata = from_namedtuple(sample_nts; sample_stats=stan_stats_nts_rekey)
-
-    if posterior_predictive_symbol in keys(stan_nts)
+    # Merge both log_likelihood and posterior_predictive groups into idata if present
+    if !isnothing(posterior_predictive_symbol) && posterior_predictive_symbol in keys(stan_nts)
         nt = (y = stan_nts[posterior_predictive_symbol],)
         idata = merge(idata, from_namedtuple(nt; posterior_predictive = (:y,)))
     end
 
-    if log_likelihood_symbol in keys(stan_nts)
+    if !isnothing(log_likelihood_symbol) log_likelihood_symbol in keys(stan_nts)
         nt = (y = stan_nts[log_likelihood_symbol],)
         idata = merge(idata, from_namedtuple(nt; log_likelihood = (:y,)))
     end
 
+    # Extract warmup values in separate groups
     if include_warmup
         idata = let
             idata_warmup = idata[draw=1:1000]
@@ -53,6 +62,10 @@ function inferencedata(m::SampleModel;
             merge(idata_postwarmup, idata_warmup_rename)
         end
     end
+
+    # TO DO: update the indexing
+
+    # TO DO: add other groups (data, etc.)
 
     return idata
 end
