3823f582339e464e0edca8e0db0e368aab0e3469
Rob J Goedman <goedman@icloud.com>
2023-11-09 12:02:43 -0700

    Rel 7.5.0 - Fix for InferenceObjects with array variables

--- src/utils/namedtuples.jl
+++ src/utils/namedtuples.jl
@@ -23,6 +23,8 @@ function extract(chns::Array{Float64,3}, cnames::Vector{String}; permute_dims=fa
         end
     end
 
+    #println(group_map)
+
     for (name, group) in group_map
         max_idx = maximum(hcat([idx for (i, idx) in group]...), dims=2)[:,1]
         ex_dict[name] = similar(chns, max_idx..., draws, chains)
@@ -37,10 +39,21 @@ function extract(chns::Array{Float64,3}, cnames::Vector{String}; permute_dims=fa
     if permute_dims
         for key in keys(ex_dict)
             if length(size(ex_dict[key])) > 2
-                ex_dict[key] = permutedims(ex_dict[key], (2, 3, 1))
+                tmp = 1:length(size(ex_dict[key]))
+                perm = (tmp[end-1], tmp[end], tmp[1:end-2]...)
+                #println(perm)
+                ex_dict[key] = permutedims(ex_dict[key], perm)
             end
+            #=
+            if length(size(ex_dict[key])) == 3
+                ex_dict[key] = permutedims(ex_dict[key], (2, 3, 1))
+            elseif length(size(ex_dict[key])) == 4
+                ex_dict[key] = permutedims(ex_dict[key], (3, 4, 1, 2))
+            end
+            =#
         end
     end
+
 
     for name in keys(ex_dict)
         if name in [:treedepth__, :n_leapfrog__]
