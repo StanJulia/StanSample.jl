fde2fd0f950933df0ed173c4b1edad5b89dad017
Alex Keil <alexpkeil1@users.noreply.github.com>
2025-03-28 17:26:59 -0400

    Update test_metric_file.jl
    added assert condition to ensure that the metric file is included in call

--- test/test_metric_file/test_metric_file.jl
+++ test/test_metric_file/test_metric_file.jl
@@ -16,21 +16,30 @@ model {
 }
 ";
 
-fl = joinpath(tempdir(), "bernoulli.diag_e.json")
+metric_file_name = "bernoulli.diag_e.json"
 
-open(fl, "w") do f
-  write(f, "{ \"inv_metric\" : [0.296291] }")
+
+open(joinpath(tempdir(), metric_file_name), "w") do f
+    write(f, "{ \"inv_metric\" : [0.296291] }")
 end
 
 
 data = Dict("N" => 10, "y" => [0, 1, 0, 1, 0, 0, 0, 0, 0, 1])
 sm = SampleModel("bernoulli", bernoulli_model);
 
-rc = stan_sample(sm; data, algorithm=:hmc, stepsize=0.9, metric_file=fl);
+rc = stan_sample(
+    sm;
+    data,
+    algorithm = :hmc,
+    stepsize = 0.9,
+    metric_file = fl,
+    num_warmups = 0,
+    engaged = false,
+);
 
 if success(rc)
-  (samples, cnames) = read_samples(sm, :array; return_parameters=true)
-
-  sdf = read_summary(sm)rm 
-  sdf |> display
+    (samples, cnames) = read_samples(sm, :array; return_parameters = true)
+    @assert occursin(metric_file_name, string(rc.processes[1].cmd))
+    sdf = read_summary(sm)rm
+    sdf |> display
 end
